5a0d4243a45392b6dbda67630a5b7a5eff547abc
Aakash Shah <bca64sem1ddu@gmail.com>
2023-01-12 17:54:04 +0530

    welcome

--- models/notification/App.js
+++ models/notification/App.js
@@ -1,23 +1,47 @@
 /* eslint-disable prettier/prettier */
-import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect,useState } from 'react';
+import { Text, View,FlatList,Image, StatusBar,Animated,Dimensions } from 'react-native';
 import {requestUserPermission,NotificationListener} from "./src/utils/pushnotification_helper"
 
+const {width,height} = Dimensions.get('screen');
+
+
 const App = () => {
+  const [data, setData] = useState([]);
 
   useEffect(()=>{
+
+    fetch('https://theaakashprojects.cf/welcome')
+      .then(response => response.json())
+      .then(data => 
+        setData(data.data))
+      .catch(error => console.log(error));
+
     requestUserPermission();
     NotificationListener();
   },[])
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello, world!</Text>
+
+    <View style={{width}}>
+         <StatusBar hidden />
+      <Animated.FlatList
+      data={data}
+      horizontal
+      scrollEventThrottle={32}
+      pagingEnabled
+      showsHorizontalScrollIndicator={false}
+      renderItem={({ item }) => 
+      <View>
+
+          <Image
+            source={{uri: item.image}}
+            style={{width,height}}
+          />
+        </View>
+        }
+      keyExtractor={item => item.id}
+    />
     </View>
   )
 }
